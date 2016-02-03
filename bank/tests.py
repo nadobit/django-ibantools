3a52962cd6bf9b65fd423913d57211830c17b54c
Bastian Probian <contmp@me.com>
2016-02-03 21:54:27 +0100

    Iban Generierung mit Tests

--- bank/tests.py
+++ bank/tests.py
@@ -21,9 +21,6 @@ class ValidateBicTestCase(TestCase):
 
 class ValidateIBANTestCase(TestCase):
 
-    def setUp(self):
-        pass
-
     def test_valid_iban_number(self):
         self.assertTrue(utils.validate_iban('DE08700901001234567890'))
 
@@ -31,3 +28,48 @@ class ValidateIBANTestCase(TestCase):
         self.assertFalse(utils.validate_iban('DE08700901001234567891'))
         self.assertFalse(utils.validate_iban('asd!'))
         self.assertFalse(utils.validate_iban('asd'))
+
+
+class ValidateIBANGenerationTestCase(TestCase):
+
+    def test_success_generation(self):
+        self.assertEquals(
+            utils.get_iban_by_bank_account(
+                account_number='70001506',
+                bank_number='70000000'),
+            'DE05700000000070001506'
+        )
+
+    def test_unsupported_country(self):
+        with self.assertRaises(ValueError):
+            utils.get_iban_by_bank_account(
+                country='US',
+                account_number='70001506',
+                bank_number='70000000'
+            )
+
+    def test_invalid_account_numbers(self):
+        with self.assertRaises(ValueError):
+            utils.get_iban_by_bank_account(
+                account_number='!123123132123',
+                bank_number='70000000'
+            )
+
+        with self.assertRaises(ValueError):
+            utils.get_iban_by_bank_account(
+                account_number='123',
+                bank_number='70000000'
+            )
+
+    def test_invalid_bank_numbers(self):
+        with self.assertRaises(ValueError):
+            utils.get_iban_by_bank_account(
+                account_number='70001506',
+                bank_number='!70000000'
+            )
+
+        with self.assertRaises(ValueError):
+            utils.get_iban_by_bank_account(
+                account_number='70001506',
+                bank_number='1'
+            )
--- bank/utils.py
+++ bank/utils.py
@@ -29,3 +29,27 @@ def validate_iban(iban):
     mod_result = checksum % 97
 
     return mod_result == 1
+
+
+def get_iban_by_bank_account(account_number, bank_number, country='DE'):
+
+    if country != 'DE':
+        raise ValueError('This version only supports german iban number generation')
+
+    if not re.match('^[0-9]{6,10}$', account_number):
+        raise ValueError('Unknown account number type')
+
+    if not re.match('^[0-9]{8}$', bank_number):
+        raise ValueError('Unknown bank number type')
+
+    account_number = "%010d" % int(account_number)
+    bban = "%s%s" % (bank_number, account_number)
+    country_code = operations.get_numeric_country_code(country)
+    country_code_ex = "%s00" % country_code
+    checksum = int("%s%s" % (bban, country_code_ex))
+    mod_result = checksum % 97
+    mod_checksum = "%02d" % (98 - mod_result)
+
+    iban = "%s%s%s" % (country, mod_checksum, bban)
+
+    return iban
