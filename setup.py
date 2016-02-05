37ea569e72242aa5687ecaf10a57fae353c4bcde
Bastian Probian <contmp@me.com>
2016-02-05 19:10:08 +0100

    Update Setup

--- setup.py
+++ setup.py
@@ -9,13 +9,13 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-ibantools',
-    version='0.1',
+    version='0.2',
     packages=['bank'],
     include_package_data=True,
     license='BSD License',
     description='A simple IBAN/Bic Datamanager app',
     long_description=README,
-    url='https://nadobit.de/',
+    url='https://github.com/nadobit/django-ibantools',
     author='Bastian Probian',
     author_email='contmp@me.com',
     classifiers=[
